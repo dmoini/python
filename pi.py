73e81737a12ccd1171f7fd79cad5cd4c5543f022
Donovan Moini <donnym99@gmail.com>
2018-08-07 18:23:50 -0700

    Create pi.py

---
+++ pi.py
@@ -0,0 +1,28 @@
+from decimal import Decimal
+from decimal import getcontext
+import math
+
+# https://en.wikipedia.org/wiki/Chudnovsky_algorithm
+def chudnovskyPi(precision):
+    pi = Decimal(0)
+    k = 0
+    while k < precision:
+        M = Decimal(math.factorial(6 * k)) / (Decimal(math.factorial(3 * k)) * Decimal(math.factorial(k)**3))
+        L = Decimal((545140134 * k) + 13591409)
+        X = Decimal(-262537412640768000**k)
+        pi += (M * L) / X
+        k += 1
+    pi = pi**(-1)
+    pi *= Decimal(426880) * Decimal(10005).sqrt()
+    return pi * -1
+
+def pi(precision=10):
+    if precision > 100:
+        print("This only goes up to 100 digits. Please input a new precision that is less than 100.")
+    else:
+        getcontext().prec = precision
+        print(chudnovskyPi(precision))
+
+
+precision = int(input("How many digits of pi would you like to compute? (100 max): "))
+pi(precision)
